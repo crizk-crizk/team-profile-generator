9a02b4624d842e1ea929e1aa18f4026d2230feca
Carolina R <carolinarizk@gmail.com>
2021-02-06 11:56:05 -0500

    added add engineer function, add intern, next move

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const Intern = require("./lib/Intern.js");
 
 //storage class data
 const data = { manager: null, engineers: [], interns: [] };
-let finishPrompts = false;
 
 // Questions for manager
 inquirer
@@ -41,6 +40,7 @@ inquirer
     );
     console.log("manager: ", newManager);
     data.manager = newManager;
+    promptNextMove();
   })
   .catch((error) => {
     if (error.isTtyError) {
@@ -50,33 +50,130 @@ inquirer
     }
   });
 
-  //stop building team until user chooses to end prompts
-  function promptNextMove () {
-    // Questions for additional employee
-inquirer
-.prompt([
-  {
-    type: "list",
-    name: "nextStep",
-    message: "Choose the next step",
-    choices: ['Add engineer', 'Add intern', 'Done']
-  },
-])
-.then((choice) => {
-  // Instance of manager
-  const userDecision = choice.nextStep;
-  if (userDecision === 'Done') {
-    finishPrompts = true;
-  } else {
-      
-  }
-})
-.catch((error) => {
-  if (error.isTtyError) {
-    // Prompt couldn't be rendered in the current environment
-  } else {
-    // Something else went wrong
-  }
-});
+//add engineer
+function addEngineer() {
+  // Questions for additional employee
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Enter engineer name ",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Enter engineer ID",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter engineer email address ",
+      },
+      {
+        type: "input",
+        name: "gitHub",
+        message: "Enter engineer GitHub user name ",
+      },
+    ])
+    .then((engineer) => {
+      // Instance of engineer
+      const newEngineer = new Engineer(
+        engineer.name,
+        engineer.id,
+        engineer.email,
+        engineer.gitHub
+      );
+      console.log(newEngineer);
+      data.engineers.push(newEngineer);
+      promptNextMove();
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
+      }
+    });
+}
 
-  }
+//add intern
+function addIntern() {
+  // Questions for additional employee
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Enter intern name ",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Enter intern ID",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter intern email address ",
+      },
+      {
+        type: "input",
+        name: "gitHub",
+        message: "Enter intern school name ",
+      },
+    ])
+    .then((intern) => {
+      // Instance of engineer
+      const newIntern = new Intern(
+        intern.name,
+        intern.id,
+        intern.email,
+        intern.gitHub
+      );
+      console.log(newIntern);
+      data.interns.push(newIntern);
+      promptNextMove();
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
+      }
+    });
+}
+
+//prompt next move
+function promptNextMove() {
+  // Questions for enginneer
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "nextStep",
+        message: "Choose the next step",
+        choices: ["Add engineer", "Add intern", "Done"],
+      },
+    ])
+    .then((choice) => {
+      // Instance of manager
+      const userDecision = choice.nextStep;
+      if (userDecision === "Add engineer") {
+        addEngineer();
+      } else if (userDecision === "Add intern") {
+        addIntern();
+      } else {
+        console.log(data);
+        // call build HTML function here
+      }
+    })
+
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
+      }
+    });
+}
